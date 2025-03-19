ed7e7ed1c01c53b6dce466652f01c9061debbcc4
julian <juliansibbing@web.de>
2025-03-19 20:36:28 +0100

    setState sample

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,10 +1,21 @@
+'use client'
+
 import Link from "next/link";
+import { useState } from "react";
 
 export default function Dashboard() {
-  return (
-    <div className="flex flex-col">
-        This is the dashboard!
-        <Link href="/">back to main page</Link>
-    </div>
-  );
+    const [count, setCount] = useState(0);
+
+
+    return (
+        <div className="flex flex-col">
+            This is the dashboard!
+            <p>You clicked {count} times</p>
+            <button onClick={() => setCount(count + 1)}>
+                Click me
+            </button>
+
+            <Link href="/">back to main page</Link>
+        </div>
+    );
 }
